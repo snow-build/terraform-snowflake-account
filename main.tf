e98f308134b4a7e00b22395b400494321f4d4309
cvchakradharreddy <cvchakradharreddy@gmail.com>
2024-05-17 02:25:49 -0400

    Fixing warehouse list issue

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "snowflake" {
 
 resource "snowflake_warehouse" "all_warehouses" {
   provider       = snowflake.sysadmin
-  for_each       = { for idx, wh in var.warehouses : wh.name => wh }
+  for_each       = { for idx, wh in local.warehouses : wh.name => wh }
   name           = each.value.name
   warehouse_size = each.value.warehouse_size
   comment        = can(each.value.comment) ? each.value.comment : null
